b66910b7c9ac610c0f557fb6d60fbbe00f465f22
oabuhamdan <osamaabuhamdan@yahoo.com>
2025-02-17 08:24:25 -0600

    Cleanup

--- MininetFederatedLearning/plot_network_logs.py
+++ MininetFederatedLearning/plot_network_logs.py
@@ -2,38 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-DIR = "logs/[withBW]_flowsched_cubic_0115_164245"
-
-
 # Load data for all 4 clients
 def plot_network_logs(DIR, type="fl", number_of_clients=5, offset=0):
     def get_data(node):
-        data = pd.read_csv(f"logs/{DIR}{'/iperf_logs' if type =='bgclient' else ''}/{node}_network.csv", skiprows=2,
-                           names=["time", "rx_rate", "tx_rate"], delimiter='\s+', usecols=["rx_rate", "tx_rate"])
+        data = pd.read_csv(f"logs/{DIR}{'/iperf_logs' if type =='bgclient' else ''}/{node}_network.csv", usecols=["rx_rate_bps", "tx_rate_bps"])
         return data[data >= 0]
 
-    number_of_clients+= 1 if type =='bgclient' else 0
-    fig, axs = plt.subplots(number_of_clients , 1, figsize=(10, 15))  # 2x2 grid of subplots
+    fig, axs = plt.subplots(number_of_clients + (1 if type=="flclient" else 0) , 1, figsize=(10, 15))  # 2x2 grid of subplots
 
     # Plot data for each client in a different subplot
     for i in range(number_of_clients):
         client_data = get_data(f"{type}{offset+i}")
-        client_data["tx_rate"] = client_data["tx_rate"] // 1024
-        client_data["rx_rate"] = client_data["rx_rate"] // 1024
-        axs[i].plot(client_data, label=["tx_rate", "rx_rate"])
+        client_data["tx_rate_bps"] = client_data["tx_rate_bps"] // (1024*1024)
+        client_data["rx_rate_bps"] = client_data["rx_rate_bps"] // (1024*1024)
+        axs[i].plot(client_data, label=["rx_rate", "tx_rate"])
         axs[i].legend()
         axs[i].grid()
         axs[i].set_title(f"Client {offset+i}")
-    if type != "bgclient":
+    if type == "flclient":
         server = get_data("server")
-        server["tx_rate"] = server["tx_rate"] // 1024
-        server["rx_rate"] = server["rx_rate"] // 1024
-        axs[-1].plot(server, label=["tx_rate", "rx_rate"])
+        server["tx_rate_bps"] = server["tx_rate_bps"] // (1024 * 1024)
+        server["rx_rate_bps"] = server["rx_rate_bps"] // (1024 * 1024)
+        axs[-1].plot(server, label=["rx_rate", "tx_rate"])
         axs[-1].grid()
+        axs[-1].legend()
         axs[-1].set_title("Server")
 
     plt.tight_layout()
-    # plt.show()
-    plt.savefig(f"logs/plots/{DIR}_netlogs.png")
+    plt.show()
+    # plt.savefig(f"logs/plots/{DIR}_netlogs.png")
 
-plot_network_logs("[withBW]_test_networkstat_0203_214908", type="flclient_", number_of_clients=5, offset=0)
+plot_network_logs("[withBW]_agfs_15rounds_g25_70212_174326", type="flclient", number_of_clients=3, offset=14)
