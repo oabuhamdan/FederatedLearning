43162c3b320d2978c654a24a9524b99f0c774b15
oabuhamdan <osamaabuhamdan@yahoo.com>
2025-04-08 13:09:33 -0500

    Start counting at 1

--- MininetFederatedLearning/experimentscode/prepare_dataset.py
+++ MininetFederatedLearning/experimentscode/prepare_dataset.py
@@ -8,6 +8,6 @@ partitioner = IidPartitioner(num_partitions=NUM_CLIENTS)
 fds = FederatedDataset(dataset=DATASET, partitioners={"train": partitioner, "test": 1})
 for partition_id in range(NUM_CLIENTS):
     partition = fds.load_partition(partition_id, "train")
-    partition.save_to_disk(f"data/{DATASET}/client_{partition_id}_data")
+    partition.save_to_disk(f"data/{DATASET}/client_{partition_id + 1}_data")
 testset = fds.load_split("test")
 testset.save_to_disk(f"data/{DATASET}/server_val_data")
