d00bbe378f5e2c046e52af291f79a0e8c553f171
oabuhamdan <osamaabuhamdan@yahoo.com>
2025-04-17 20:33:40 -0500

    Update code

--- MininetFederatedLearning/experimentscode/prepare_dataset.py
+++ MininetFederatedLearning/experimentscode/prepare_dataset.py
@@ -1,13 +1,22 @@
+import argparse
+
 from flwr_datasets import FederatedDataset
 from flwr_datasets.partitioner import IidPartitioner
 
-NUM_CLIENTS = 10
-DATASET = 'cifar10'
 
-partitioner = IidPartitioner(num_partitions=NUM_CLIENTS)
-fds = FederatedDataset(dataset=DATASET, partitioners={"train": partitioner, "test": 1})
-for partition_id in range(NUM_CLIENTS):
-    partition = fds.load_partition(partition_id, "train")
-    partition.save_to_disk(f"data/{DATASET}/client_{partition_id + 1}_data")
-testset = fds.load_split("test")
-testset.save_to_disk(f"data/{DATASET}/server_val_data")
+def prepare_dataset():
+    partitioner = IidPartitioner(num_partitions=args.num_clients)
+    fds = FederatedDataset(dataset=args.dataset, partitioners={"train": partitioner, "test": 1})
+    for partition_id in range(args.num_clients):
+        partition = fds.load_partition(partition_id, "train")
+        partition.save_to_disk(f"data/{args.dataset}/client_{partition_id + 1}_data")
+    testset = fds.load_split("test")
+    testset.save_to_disk(f"data/{args.dataset}/server_val_data")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Prepare dataset for federated learning")
+    parser.add_argument("-c", "--num-clients", type=int, default=10, help="Number of clients")
+    parser.add_argument("-d", "--dataset", type=str, default="cifar10", help="dataset")
+    args = parser.parse_args()
+    prepare_dataset()
