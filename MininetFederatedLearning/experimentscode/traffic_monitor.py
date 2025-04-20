3b7c950967e4ef21dde9576900c0f4badd50c750
oabuhamdan <osamaabuhamdan@yahoo.com>
2025-04-19 20:45:28 -0500

    Code Fix

--- MininetFederatedLearning/experimentscode/traffic_monitor.py
+++ MininetFederatedLearning/experimentscode/traffic_monitor.py
@@ -1,25 +1,22 @@
 import sys
 import csv
 import threading
-from scapy.all import sniff, IP, TCP, UDP
+import time
+
+from scapy.all import sniff, IP, TCP, UDP, AsyncSniffer
 from collections import defaultdict
 import signal
 
-# Traffic data container
 traffic_data = defaultdict(lambda: {"packets": 0, "bytes": 0})
-
-# Lock for thread safety
 data_lock = threading.Lock()
-# File name passed via CLI
 output_file = None
 logged = False
-
 
 def handle_sigterm(signum, frame):
     print(f"Caught {signal.Signals(signum).name}, shutting down.")
     log_to_csv_periodically(output_file)
-    sys.exit(0)  # Exit cleanly
-
+    time.sleep(2)
+    sys.exit(0)
 
 # Register the signal handler
 signal.signal(signal.SIGTERM, handle_sigterm)
@@ -57,6 +54,7 @@ def log_to_csv_periodically(file):
         with open(file, "w") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["Src IP", "Dst IP", "Src Port", "Dst Port", "Protocol", "Packets", "Bytes"])
+            print("[*] Logging traffic data to CSV...")
             for (src_ip, dst_ip, src_port, dst_port, proto), data in traffic_data.items():
                 writer.writerow([
                     src_ip,
